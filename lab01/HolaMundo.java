a57e6aff3ade30893c0d6ae990fbe3a266cacea4
Jordy Tejada <jtejadala@unsa.edu.pe>
2023-05-26 15:03:28 -0500

    Modifiqué para entregar una Bienvenida al Curso

--- lab01/HolaMundo.java
+++ lab01/HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Bienvenido al Curso de Estructura de Datos y Algoritmos");
     }
 }
